5a92dd40627714fba3c372fe24ec7e23cd86db46
Nev3rM1nd3 <panorg264@gmail.com>
2026-07-20 21:53:46 +0300

    chore: initialize Spring Boot project with Docker and PostgreSQL

---
+++ src/main/java/gr/bandmanager/BandmanagerApplication.java
@@ -0,0 +1,13 @@
+package gr.bandmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BandmanagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BandmanagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/gr/bandmanager/BandmanagerApplicationTests.java
@@ -0,0 +1,13 @@
+package gr.bandmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BandmanagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
