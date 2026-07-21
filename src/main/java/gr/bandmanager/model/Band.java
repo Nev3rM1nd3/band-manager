f180844fa4c63498aa0b8b2a6017b030464ae2ee
Nev3rM1nd3 <panorg264@gmail.com>
2026-07-21 12:12:15 +0300

    feat(band): add Band entity

---
+++ src/main/java/gr/bandmanager/model/Band.java
@@ -0,0 +1,36 @@
+package gr.bandmanager.model;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Table(name = "bands")
+@Getter
+@Setter
+@NoArgsConstructor
+public class Band extends AbstractEntity {
+
+    @NotBlank
+    @Size(max = 100)
+    @Column(name = "name", nullable = false, length = 100)
+    private String name;
+
+    @Size(max = 1000)
+    @Column(name = "description", length = 1000)
+    private String description;
+
+    @ElementCollection
+    @CollectionTable(
+            name = "band_genres",
+            joinColumns = @JoinColumn(name = "band_id")
+    )
+    @Column(name = "genre", nullable = false, length = 50)
+    private Set<String> genres = new HashSet<>();
+}
