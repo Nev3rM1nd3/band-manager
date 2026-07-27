d7c591060ffd361142be31ae65fa2bd8cce7dcde
Nev3rM1nd3 <panorg264@gmail.com>
2026-07-27 20:06:37 +0300

    feat(band-member): add mapping methods

--- src/main/java/gr/bandmanager/mapper/Mapper.java
+++ src/main/java/gr/bandmanager/mapper/Mapper.java
@@ -2,10 +2,13 @@ package gr.bandmanager.mapper;
 
 import gr.bandmanager.dto.*;
 import gr.bandmanager.model.Band;
+import gr.bandmanager.model.BandMember;
 import gr.bandmanager.model.Song;
 import gr.bandmanager.model.User;
 import gr.bandmanager.model.enums.ApplicationRole;
 import org.springframework.stereotype.Component;
+
+import java.util.Set;
 
 @Component
 public class Mapper {
@@ -105,5 +108,66 @@ public class Mapper {
                 user.getApplicationRole(),
                 user.isEnabled()
         );
+    }
+
+    public BandMember mapToBandMemberEntity(
+            BandMemberInsertDTO dto,
+            Band band,
+            User user
+    ) {
+        BandMember bandMember = new BandMember();
+
+        bandMember.setFirstname(dto.firstname());
+        bandMember.setLastname(dto.lastname());
+        bandMember.setPosition(dto.position());
+        bandMember.setBandRole(dto.bandRole());
+        bandMember.setBand(band);
+        bandMember.setUser(user);
+
+        if (dto.instruments() != null) {
+            dto.instruments().forEach(bandMember::addInstrument);
+        }
+
+        return bandMember;
+    }
+
+    public BandMemberReadOnlyDTO mapToBandMemberReadOnlyDTO(
+            BandMember bandMember
+    ) {
+        User user = bandMember.getUser();
+
+        return new BandMemberReadOnlyDTO(
+                bandMember.getId(),
+                bandMember.getFirstname(),
+                bandMember.getLastname(),
+                bandMember.getPosition(),
+                Set.copyOf(bandMember.getInstruments()),
+                bandMember.getBandRole(),
+
+                bandMember.getBand().getId(),
+                bandMember.getBand().getName(),
+
+                user != null ? user.getId() : null,
+                user != null ? user.getEmail() : null,
+
+                bandMember.getCreatedAt(),
+                bandMember.getUpdatedAt()
+        );
+    }
+
+    public void updateBandMemberFromDTO(
+            BandMemberUpdateDTO dto,
+            BandMember bandMember
+    ) {
+        bandMember.setFirstname(dto.firstname());
+        bandMember.setLastname(dto.lastname());
+        bandMember.setPosition(dto.position());
+        bandMember.setBandRole(dto.bandRole());
+
+        bandMember.getInstruments().clear();
+
+        if (dto.instruments() != null) {
+            dto.instruments().forEach(bandMember::addInstrument);
+        }
     }
 }
