769231f74257270cccfe5c8a7341b658821bcd9b
Nev3rM1nd3 <panorg264@gmail.com>
2026-08-06 14:12:01 +0300

    feat: add protected routes

--- bandmanager-frontend/src/routes/AppRouter.tsx
+++ bandmanager-frontend/src/routes/AppRouter.tsx
@@ -3,12 +3,15 @@ import AuthLayout from '../layouts/AuthLayout'
 import HomePage from '../pages/HomePage'
 import LoginPage from '../pages/LoginPage'
 import RegisterPage from '../pages/RegisterPage'
+import ProtectedRoute from './ProtectedRoute'
 
 const AppRouter = () => {
   return (
     <>
       <Routes>
-        <Route path="/" element={<HomePage />} />
+        <Route element={<ProtectedRoute />}>
+          <Route path="/" element={<HomePage />} />
+        </Route>
 
         <Route element={<AuthLayout />}>
           <Route path="/login" element={<LoginPage />} />
